94692c74eea338a7ba8dcd9aba407057715c96d8
Arjun Yadav <arjun149133@gmail.com>
2024-11-19 00:45:47 +0530

    chess board assets

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -1,4 +1,5 @@
 import { Chess, Color, PieceSymbol, Square } from "chess.js";
+import Image from "next/image";
 import { useState } from "react";
 
 const Board = ({
@@ -55,6 +56,49 @@ const Board = ({
     }
   };
 
+  function fill(type: string, color: string): string {
+    switch (type) {
+      case "p":
+        if (color === "w") {
+          return "/white-pawn.svg";
+        } else {
+          return "/pawn.svg";
+        }
+      case "k":
+        if (color === "w") {
+          return "/white-king.svg";
+        } else {
+          return "/king.svg";
+        }
+      case "q":
+        if (color === "w") {
+          return "/white-queen.svg";
+        } else {
+          return "/queen.svg";
+        }
+      case "r":
+        if (color === "w") {
+          return "/white-rook.svg";
+        } else {
+          return "/rook.svg";
+        }
+      case "n":
+        if (color === "w") {
+          return "/white-knight.svg";
+        } else {
+          return "/knight.svg";
+        }
+      case "b":
+        if (color === "w") {
+          return "/white-bishop.svg";
+        } else {
+          return "/bishop.svg";
+        }
+    }
+
+    return "/pawn.svg";
+  }
+
   return (
     <div className=" hover:cursor-pointer">
       {board.map((row, i) => (
@@ -69,11 +113,20 @@ const Board = ({
             >
               {square ? (
                 <div
-                  className={`text-xl ${
+                  className={`text-xl flex justify-center items-center ${
                     square.color === "w" ? "text-white" : "text-black"
                   }`}
                 >
-                  {square.type}
+                  {fill(square.type, square.color) !== null ? (
+                    <Image
+                      src={fill(square.type, square.color)}
+                      alt={square.type}
+                      width={48}
+                      height={48}
+                    />
+                  ) : (
+                    square.type
+                  )}
                 </div>
               ) : null}
             </button>
